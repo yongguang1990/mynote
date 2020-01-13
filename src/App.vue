86931e25e3ed402bb495d5eb6ae01332aeb9e0d4
张永光 <zhangyongguang@clbao.com>
2020-01-13 13:34:54 +0800

    初次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,446 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row
+            v-if="item.heading"
+            :key="item.heading"
+            align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-center"
+            >
+              <a
+                href="#!"
+                class="body-2 black--text"
+              >EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item
+              v-for="(child, i) in item.children"
+              :key="i"
+              link
+            >
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item
+            v-else
+            :key="item.text"
+            link
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">时间笔记</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="搜索笔记"
+        class="hidden-sm-and-down"
+      />
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          /></v-avatar>
+      </v-btn>
+    </v-app-bar>
+    //主内容区
+    <v-timeline :dense="$vuetify.breakpoint.smAndDown"
+    >
+    <v-timeline-item
+      color="purple lighten-2"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="purple lighten-2">
+          <v-icon
+            dark
+            size="42"
+            class="mr-4"
+          >
+            mdi-magnify
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 1</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="10">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col
+              class="hidden-sm-and-down text-right"
+              md="2"
+            >
+              <v-icon size="64">mdi-calendar-text</v-icon>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="amber lighten-1"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="amber lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Title 2</h2>
+          <v-icon
+            dark
+            size="42"
+          >mdi-home-outline</v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="8">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col cols="12" md="4">
+              Lorem ipsum dolor sit amet, no nam oblique veritus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="cyan lighten-1"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="cyan lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+            mdi-email-outline
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 3</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="n in 3"
+              :key="n"
+              cols="12"
+              md="4"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus no nam oblique.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="red lighten-1"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="red lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Title 4</h2>
+          <v-icon
+            dark
+            size="42"
+          >
+            mdi-account-multiple-outline
+          </v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col class="hidden-sm-and-down" md="2">
+              <v-icon size="64">mdi-server-network</v-icon>
+            </v-col>
+            <v-col cols="12" md="10">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="green lighten-1"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="green lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+            mdi-phone-in-talk
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 5</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col>
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                :href="source"
+                icon
+                large
+                target="_blank"
+                v-on="on"
+              >
+                <v-icon large>mdi-code-tags</v-icon>
+              </v-btn>
+            </template>
+            <span>Source</span>
+          </v-tooltip>
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                large
+                href="https://codepen.io/johnjleider/pen/MNYLdL"
+                target="_blank"
+                v-on="on"
+              >
+                <v-icon large>mdi-codepen</v-icon>
+              </v-btn>
+            </template>
+            <span>Codepen</span>
+          </v-tooltip>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-btn
+      bottom
+      color="pink"
+      dark
+      fab
+      fixed
+      right
+      @click="dialog = !dialog"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog
+      v-model="dialog"
+      width="800px"
+    >
+      <v-card>
+        <v-card-title class="grey darken-2">
+          Create contact
+        </v-card-title>
+        <v-container>
+          <v-row class="mx-2">
+            <v-col
+              class="align-center justify-space-between"
+              cols="12"
+            >
+              <v-row
+                align="center"
+                class="mr-0"
+              >
+                <v-avatar
+                  size="40px"
+                  class="mx-3"
+                >
+                  <img
+                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
+                    alt=""
+                  >
+                </v-avatar>
+                <v-text-field
+                  placeholder="Name"
+                />
+              </v-row>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                prepend-icon="business"
+                placeholder="Company"
+              />
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                placeholder="Job title"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                prepend-icon="mail"
+                placeholder="Email"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                type="tel"
+                prepend-icon="phone"
+                placeholder="(000) 000 - 0000"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                prepend-icon="notes"
+                placeholder="Notes"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-card-actions>
+          <v-btn
+            text
+            color="primary"
+          >More</v-btn>
+          <v-spacer />
+          <v-btn
+            text
+            color="primary"
+            @click="dialog = false"
+          >Cancel</v-btn>
+          <v-btn
+            text
+            @click="dialog = false"
+          >Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    dialog: false,
+    drawer: false,
+    items: [
+      { icon: 'mdi-contacts', text: '我的日记' },
+      { icon: 'mdi-history', text: '日记统计' },
+      { icon: 'mdi-content-copy', text: '年度报告' },
+      {
+        icon: 'mdi-chevron-up',
+        'icon-alt': 'mdi-chevron-down',
+        text: 'Labels',
+        model: true,
+        children: [
+          { icon: 'mdi-plus', text: 'Create label' }
+        ]
+      },
+      { icon: 'mdi-chevron-up',
+        'icon-alt': 'mdi-chevron-down',
+        text: 'More',
+        model: false,
+        children: [
+          { text: 'Import' },
+          { text: 'Export' },
+          { text: 'Print' },
+          { text: 'Undo changes' },
+          { text: 'Other contacts' }
+        ]
+      },
+      { icon: 'mdi-settings', text: 'Settings' },
+      { icon: 'mdi-message', text: 'Send feedback' },
+      { icon: 'mdi-help-circle', text: 'Help' },
+      { icon: 'mdi-cellphone-link', text: 'App downloads' },
+      { icon: 'mdi-keyboard', text: 'Go to the old version' }
+    ]
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
